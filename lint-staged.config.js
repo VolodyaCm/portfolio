4ba382f28b060f16258f1b3b693831de72507960
Volodymyr Smal-Stotskiy <volodyacm@gmail.com>
2022-02-27 15:21:42 +0200

    Add stylelint

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,10 +2,13 @@ module.exports = {
   // Type check TypeScript files
   '**/*.(ts|tsx)': () => 'npm run check-types',
 
-  // Lint then format TypeScript and JavaScript files
+  // Lint TypeScript and JavaScript files
   '**/*.(ts|tsx|js)': () => [`npm run check-format`, `npm run check-lint`],
 
-  // Format MarkDown and JSON
+  // Check MarkDown and JSON
   '**/*.(md|json)': (filenames) =>
     `npx prettier --check ${filenames.join(' ')}`,
+
+  // Lint scss
+  '**/*.(scss)': () => [`npm run check-lint-styles`],
 };
