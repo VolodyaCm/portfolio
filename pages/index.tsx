108bb5e217b84d7d592fbf0a3f72a33393da5bdf
Volodymyr Smal-Stotskiy <volodyacm@gmail.com>
2022-03-16 20:07:38 +0200

    Add tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Htag from '@components/Htag';
 const Home: NextPage = () => {
   return (
     <div>
-      <Htag size={1}>Hello</Htag>
+      <h1 className="text-7xl">Hello</h1>
       <Htag size={2}>Hello</Htag>
       <Htag size={3}>Hello</Htag>
       <Htag size={4}>Hello</Htag>
