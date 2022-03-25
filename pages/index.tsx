cb79efc47b0a5db2a554d8344ff376b238af28de
Volodymyr Smal-Stotskiy <volodyacm@gmail.com>
2022-03-25 19:03:03 +0200

    Add basic animation for header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,21 @@
 import type { NextPage } from 'next';
 import Header from '@/containers/Header';
+import { motion } from 'framer-motion';
 
 const Home: NextPage = () => {
   return (
     <div>
-      <Header />
+      <motion.div
+        initial="hidden"
+        animate="visible"
+        transition={{ duration: 0.5 }}
+        variants={{
+          hidden: { opacity: 0 },
+          visible: { opacity: 1 },
+        }}
+      >
+        <Header />
+      </motion.div>
     </div>
   );
 };
